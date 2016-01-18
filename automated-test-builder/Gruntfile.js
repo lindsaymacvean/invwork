733d0389cc33aee38a94d76f5cfa4c03cb3c48c3
lindsaymacvean <lindsaymacvean@gmail.com>
2016-01-18 18:09:08 +0000

    updated lots of user CRUD tests, still some content types left

--- automated-test-builder/Gruntfile.js
+++ automated-test-builder/Gruntfile.js
@@ -1,4 +1,16 @@
 module.exports = function(grunt) {
+
+function rename(dest, src) {
+    var path = require('path');
+
+    // Get the name of the component folder (or first folder in src path)
+    var component = src.split(path.sep).slice(0, 1)[0];
+
+    // Prefix each javascript file with the
+    // component folder name into the destination
+    return path.join(dest, component + '_' + path.basename(src));
+}
+
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -6,17 +18,38 @@ module.exports = function(grunt) {
         	options: {
         		flatten: true,
         		layoutdir: 'test-templates/meta_test_templates',
-        		partials: ['test-templates/general_includes/**/*.hbs', 'test-templates/author/**/*.hbs', 'test-templates/content/**/*.hbs'] 
+        		partials: [
+                    'test-templates/general_includes/**/*.hbs', 
+                    'test-templates/users/**/*.hbs', 
+                    'test-templates/content/**/*.hbs'] 
         	},
+            basic_test_units: {
+                options: { layout: 'basic_test_units.hbs'},
+                dest: '../compiled-tests/individual_building_blocks',
+                src: [
+                    'test-templates/general_includes/*.hbs',
+                    'test-templates/content/**/*.hbs'
+                    ]
+            },
+            webmaster_crud: {
+                options: { layout: 'webmaster_crud.hbs' },
+                dest: '../compiled-tests/content/webmaster',
+                src: ['test-templates/content/content_types/*.hbs']
+            },
+            publisher_crud: {
+                options: { layout: 'publisher_crud.hbs' },
+                dest: '../compiled-tests/content/publisher',
+                src: ['test-templates/content/content_types/*.hbs']
+            },
+            editor_crud: {
+                options: { layout: 'editor_crud.hbs' },
+                dest: '../compiled-tests/content/editor',
+                src: ['test-templates/content/content_types/*.hbs']
+            },
         	author_crud: {
                 options: { layout: 'author_crud.hbs' },
                 dest: '../compiled-tests/content/author',
                 src: ['test-templates/content/content_types/*.hbs']
-            },
-            basic_test_units: {
-                options: { layout: 'basic_test_units.hbs'},
-                dest: '../compiled-tests',
-                src: ['test-templates/general_includes/*.hbs']
             }
         },
         clean: {
@@ -26,5 +59,6 @@ module.exports = function(grunt) {
     });
     grunt.loadNpmTasks('grunt-assemble');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.registerTask('default', ['clean', 'assemble']);
 };
